5d6d4c7599567e6e441289b3a5ce7a5cef83263b
Sergej Atamantschuk <sergatam@yandex.com>
2019-07-03 21:53:46 +0200

    feat: add bg server

---
+++ src/background/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3005
+
+// Background communication logic
+
+const { fork } = require('child_process')
+
+const backgroundProcess = fork('../../src/background/index.js')
+backgroundProcess.on('message', msg => {
+  console.log(msg)
+})
+
+app.listen(port, () => {
+  // process.send('Background server running on' + port)
+  console.info('Background server running on ' + port)
+})
+
+app.get('/', (req, res) => {
+  res.send('Background server running on ' + port)
+})
