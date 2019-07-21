79f94f9e3bf227e242ea6587a4a5d1be6feea55d
Sergej Atamantschuk <sergatam@yandex.com>
2019-07-21 16:55:51 +0200

    feat: sort items

--- src/background/eventsHandler/persistetUsersDataLoding.js
+++ src/background/eventsHandler/persistetUsersDataLoding.js
@@ -1,8 +1,34 @@
 const { SOCKET_USER_LIKES, SOCKET_USER_LIKES_ERROR } = require('../socketEvents');
 const { datastore } = require('../db');
+const { LIST_SORT_OPTION_ALPHABETIC, LIST_SORT_OPTION_NEWEST, LIST_SORT_OPTION_OLDEST, LIST_SORT_OPTION_STATUS } = require('../const');
+
+const formulateSort = sortOption => {
+  switch (sortOption) {
+    case LIST_SORT_OPTION_ALPHABETIC:
+      return {
+        title: 1
+      };
+    case LIST_SORT_OPTION_NEWEST:
+      return {
+        order: 1
+      };
+    case LIST_SORT_OPTION_OLDEST:
+      return {
+        order: -1
+      };
+    case LIST_SORT_OPTION_STATUS:
+      return {
+        status: -1
+      };
+    default:
+      return {
+        order: -1
+      };
+  }
+};
 
 const getUserLikes = async (io, { userId, title, status, sort }) => {
-  console.log('GETTING LIKES FOR USER: ', userId, title, status, sort);
+  // console.log('GETTING LIKES FOR USER: ', userId, title, status, sort);
 
   const filter = {
     type: 'likes',
@@ -14,13 +40,14 @@ const getUserLikes = async (io, { userId, title, status, sort }) => {
       {
         'user.username': new RegExp(`${title}`, 'i')
       }
-    ]
-    // title: /all/
+    ],
+    status: { $in: status }
   };
-  console.log('FILTERS:', filter);
+
+  console.log('FILTERS:', filter, formulateSort(sort));
 
   try {
-    const likes = await datastore.find(filter).sort({ order: -1 }).limit(20).exec();
+    const likes = await datastore.find(filter).sort(formulateSort(sort)).limit(20).exec();
     io.emit(SOCKET_USER_LIKES, {
       userId,
       likes
