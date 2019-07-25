7e4fcf027ce44b6c2a0d8f0df6a0559450fda206
Sergej Atamantschuk <sergatam@yandex.com>
2019-07-25 19:40:36 +0200

    feat: progress styling

--- src/components/ListsGroup/ListSyncControls.vue
+++ src/components/ListsGroup/ListSyncControls.vue
@@ -1,36 +1,37 @@
 <template>
   <div class="list-group-sync-controls text-center">
-    <div class="text-center">
+    <!--<div class="text-center">
       <h5>
         Sync your Likes
       </h5>
-    </div>
+    </div>-->
     <div class="row">
       <div class="col">
-        <q-btn round flat size="ms" color="primary" icon="sync" slot="legend-capture"/>
+        <q-btn round flat size="ms" color="primary" icon="fas fa-stream"/>
       </div>
       <div class="col">
-        <q-btn round flat size="ms" color="primary" icon="sync" slot="legend-capture"/>
+        <q-btn round flat size="ms" color="primary" icon="fas fa-stream fa-flip-horizontal"/>
       </div>
     </div>
-    <vue-ellipse-progress :progress="progress" font-size="2rem" :animation="{type: 'rs', delay: 1000, duration: 700}">
-      <span slot="legend-value">%</span>
-      <q-btn round flat size="lg" color="primary" icon="sync" slot="legend-capture"/>
-    </vue-ellipse-progress>
+    <sync-progress :progress="progress">
+      <q-btn round flat size="lg" color="primary" icon="fas fa-sync-alt"/>
+    </sync-progress>
     <div class="row">
       <div class="col">
-        <q-btn round flat size="ms" color="primary" icon="sync" slot="legend-capture"/>
+        <q-btn round flat size="ms" color="primary" icon="fas fa-angle-double-right"/>
       </div>
       <div class="col">
-        <q-btn round flat size="md" color="primary" icon="sync" slot="legend-capture"/>
+        <q-btn round flat size="ms" color="primary" icon="fas fa-angle-double-left"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import SyncProgress from 'components/Base/SyncProgress';
 export default {
   name: 'ListSyncControls',
+  components: { SyncProgress },
   props: {
     progress: {
       type: Number,
@@ -44,6 +45,8 @@ export default {
   .list-group-sync-controls {
     //position: fixed;
     //left: calc(50vw - 105px);
+    position: relative;
+    top: 30vh;
     &.fixed {
       position: fixed;
     }
